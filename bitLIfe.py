90e5828034f35e86c144bc15e84abd043cc28742
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-05-11 10:12:28 -0500

    Added age inforation

--- bitLIfe.py
+++ bitLIfe.py
@@ -1,44 +1,43 @@
 
-#While loop Information != 
+import random
 
 
 information = []
+Age= [0,1,2,3,4,5]
+Month= ["January", "February","March", "April","May", "June", "July", "August", "October", "November", "December"]
+Scope=[""]
+print(" _    _    _     _  _  ___     ")
+print("| |_ <_> _| |_  | |<_>| | '___ ")
+print("| . \| |  | |   | || || |-/ ._>")
+print("|___/|_|  |_|   |_||_||_| \___.")
+print("                               ")
 
-while True:
 
 
     # Name
-    name= input("what is your name? ")
+name= input("what is your name? ")
 
-    information.append(name)
+information.append(name)
     # gender
-    gender= input("Are You male or female? ").upper()
+gender= input("Are You male or female? ").upper()
 
-    information.append(gender)
+information.append(gender)
 
     # Country
-    country= input("Choose your country ")
+country= input("Choose your country ")
 
-    information.append(country)
+information.append(country)
 
     # Place
-    place= input("Choose the place ").upper()
+place= input("Choose the place ").upper()
 
-    information.append(place)
+information.append(place)
 
     #printed information
-    print(information)
+print(information)
      
-     #Term to decided  to end the program
-    
-    Term = str(input(" Enter q to terminate program "))
-    
-    #If statement to terminate program
-    if(Term == "q"):
-        break
-    else:
-        continue
-    
-    
+     # Age 0 information
+
+print("I was born  a ", information[1], "in" , information[3], information[2], "I was conceived while my Mother was staying at her in-laws""\n""my birthday is" , random.choice(Month), "I am a", Scope)
 
 
